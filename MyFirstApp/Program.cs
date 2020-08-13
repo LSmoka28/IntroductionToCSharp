d4b9cb7b82c961cfd094ddea23da3bf32f397525
Larry Hurley <larry3rdh@gmail.com>
2020-08-12 17:37:03 -0700

    Add first files

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* this is a simple example of WriteLine */
+           
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is for the first class exercise.");
+            Console.ReadLine();
+
+        }
+    }
+}
