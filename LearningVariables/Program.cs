0d7d61c4a2e8afc36b6098b35558d906cb9e3c4b
Larry <larry3rdh@gmail.com>
2020-08-13 11:39:51 -0700

    Learning Variables and pushing

---
+++ LearningVariables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningVariables
+{
+    class Program
+    {
+        static void Main()
+        {
+            string greeting = "Hello, user!";
+            int playerScore = 0;
+            string displayScore = "Your score currently is " + playerScore + ".";
+
+            Console.WriteLine(greeting);
+            Console.WriteLine(displayScore);
+            Console.ReadKey();
+
+        }
+    }
+}
