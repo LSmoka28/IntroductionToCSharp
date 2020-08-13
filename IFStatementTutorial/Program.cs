1db07d0a1c59658b2f64bb569394a81861b6ea16
Larry <larry3rdh@gmail.com>
2020-08-13 16:41:57 -0700

    Add if and else statement practice

---
+++ IFStatementTutorial/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFStatementTutorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            int score = 0;
+            int combo = 1;
+            int pointValue = 100;
+            bool gameStarted = false;
+
+            if (score == 0)
+            {
+                gameStarted = true;
+
+                if (gameStarted)
+                {
+                    score = combo + pointValue;
+                    combo++;
+
+                    Console.WriteLine("You got a combo! Your new score is: " + score);
+                    Console.ReadKey();
+
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
