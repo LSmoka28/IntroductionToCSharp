212f47a6bd4bb7fb2ebcd1938fbb0ff4f9d583b7
Larry <larry3rdh@gmail.com>
2020-08-13 19:34:32 -0700

    Add Loops Tutorial, Exercise, and Document

---
+++ LoopTutorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int count = 10; count < 20; count ++)
+            {
+                Console.WriteLine("The value of count is now " + count + ".");
+                if (count == 16)
+                {
+                    break;
+                }
+
+            }
+            Console.WriteLine("The loop is now finished.");
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
---
+++ LoopsExercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfDogs = 0;
+            int score = 0;
+
+            while (numberOfDogs < 10)
+            {
+               // adds one dog every loop
+                numberOfDogs++;
+                Console.WriteLine("The number of dogs in the room is now " + numberOfDogs + ".");
+
+                // add 20 to score after dogs reach 10
+                // break out after 100 score is reached
+                while (numberOfDogs == 10 && score < 100)
+                {
+                    score += 20;
+                    Console.WriteLine("The score is: " + score);
+
+                    if (score > 100)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            // adds one bag of dog food every loop
+            for (int bagsOfDogFood = 1; bagsOfDogFood <= 20 ; bagsOfDogFood++)
+            {
+                Console.WriteLine("You have " + bagsOfDogFood + " bags of dog food.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
