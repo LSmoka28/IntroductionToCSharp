ac43a0818ba4f6b823dd29f218ea8f6b82c7cfe6
Larry <larry3rdh@gmail.com>
2020-09-04 15:07:08 -0700

    Add spreadsheet for file read (WIP)

--- ConsoleProjTemp/Program.cs
+++ ConsoleProjTemp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace ConsoleProjTemp
 {
@@ -10,16 +12,102 @@ namespace ConsoleProjTemp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Marvel Store!");
+            Weapon[] shopInventory = LoadWeapon("WeaponsSpread1.csv");
+            bool gameRunning = true;
+
+
+            Console.WriteLine("Welcome to Fantasy Fanatics!");
             Console.WriteLine("Please make a selection...");
 
-            
+           while (gameRunning)
+            {
+                Console.WriteLine("What would you like to do?");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "inv":
+                        foreach (Weapon weap in shopInventory)
+                        {
+                            Console.WriteLine(weap);
+                        }
+                        break;
+
+                }
+            }
 
 
 
-
+            // selelction options: 
+            //shop- show inv of shop keeper and prompt player to view or buy
+            //---- view- shows weapon/armor stats
+            //-------weapon - displays name, type, attack power, rarity, and cost
+            //-------armor  - displays name, type, shield power, rarity, and cost
+            //inv- show inventory and wallet of player
+            //sell- show current inv and prompt player for a selection
+            //
 
             Console.ReadKey();
         }
+        private static Weapon[] LoadWeapon(string filename)
+        {
+            Weapon[] tmpArr;
+
+            string[] lines = File.ReadAllLines(filename);
+
+            tmpArr = new Weapon[lines.Length - 1];
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] lineValues = lines[i].Split(',');
+
+                if (lineValues[1] == "Melee")
+                {
+                    //Create melee weapon
+
+                    Melee tmpMelee = new Melee();
+                    tmpMelee.name = lineValues[0];
+                    tmpMelee.type = lineValues[1];
+                    tmpMelee.info = lineValues[2];
+                    tmpMelee.rarity = lineValues[4];
+
+                    if (lineValues[3] != "")
+                    {
+                        int.TryParse(lineValues[3], out tmpMelee.attackPwr);
+                    }
+                    if (lineValues[5] != "")
+                    {
+                        int.TryParse(lineValues[5], out tmpMelee.price);
+                    }
+                    tmpArr[i - 1] = tmpMelee;
+
+                }
+                else
+                {
+                    //ranged if not melee
+                    Ranged tmpRanged = new Ranged();
+                    tmpRanged.name = lineValues[0];
+                    tmpRanged.type = lineValues[1];
+                    tmpRanged.info = lineValues[2];
+                    tmpRanged.rarity = lineValues[4];
+
+                    if (lineValues[3] != "")
+                    {
+                        int.TryParse(lineValues[3], out tmpRanged.attackPwr);
+                    }
+                    if (lineValues[5] != "")
+                    {
+                        int.TryParse(lineValues[5], out tmpRanged.price);
+                    }
+                    tmpArr[i - 1] = tmpRanged;
+                }
+
+            }
+            return tmpArr;
+        }
+
+
+
     }
+
+
 }
---
+++ ConsoleProjTemp/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleProjTemp
+{
+    class Weapon
+    {
+        //-------weapon - displays name, type, info, attack power, rarity, and cost
+        public string name;
+        public string type;
+        public string info;
+        public int attackPwr;
+        public string rarity;
+        public int price;
+
+    }
+}
